72de934f2ae353f5d809d5772f771c0c14433462
ybhatia <yash.yb97@gmail.com>
2020-03-07 11:27:29 +0530

    Latest Commit

---
+++ Bus Booking Management System/src/com/cg/obtrs/dao/AdminDAO.java
@@ -0,0 +1,10 @@
+package com.cg.obtrs.dao;
+
+import com.cg.obtrs.dto.AdminDTO;
+
+public interface AdminDAO {
+
+	 public String adminSignUp(AdminDTO adminDto);
+	    
+	    public String adminLoginIn(String userName, String password);
+}
