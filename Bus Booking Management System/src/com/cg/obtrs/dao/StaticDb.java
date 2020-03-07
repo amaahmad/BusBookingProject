2a01cbe9f4c043918c1b643f65aa3cb9a75317b2
amaahmad <amaahmad@DIN52004487.corp.capgemini.com>
2020-03-07 15:03:37 +0530

    staticdb

--- Bus Booking Management System/src/com/cg/obtrs/dao/StaticDb.java
+++ Bus Booking Management System/src/com/cg/obtrs/dao/StaticDb.java
@@ -3,12 +3,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-<<<<<<< HEAD
+
 import com.cg.obtrs.dto.BookingDTO;
 import com.cg.obtrs.dto.BusDTO;
-=======
 import com.cg.obtrs.dto.AdminDTO;
->>>>>>> branch 'master' of https://github.com/amaahmad/BusBookingProject.git
+
 import com.cg.obtrs.dto.CustomerDTO;
 
 public class StaticDb 
@@ -20,27 +19,6 @@ public class StaticDb
  static public ArrayList<AdminDTO> adminArr=new ArrayList<>();
  
 
-public static ArrayList<AdminDTO> getAdminArr() {
-	return adminArr;
-}
-
-public static void setAdminArr(ArrayList<AdminDTO> adminArr) {
-	StaticDb.adminArr = adminArr;
-}
-
-
-
- static public Map<Integer, BookingDTO> bookingList = new HashMap<>();
-	
-
-public static ArrayList<CustomerDTO> getCustArr() {
-	return custArr;
-}
-
-public static void setCustArr(ArrayList<CustomerDTO> custArr) {
-	StaticDb.custArr = custArr;
-	System.out.println("");
->>>>>>> branch 'master' of https://github.com/amaahmad/BusBookingProject.git
 }
 
 
